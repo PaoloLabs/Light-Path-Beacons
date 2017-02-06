ad8e1033a0cfebf9ebb5d24a2148d021a7c70f82
luis.mantilla <lmmantilla9350@gmail.com>
2017-02-06 10:34:51 -0400

    incorporate TabBar at module Main

--- app/src/main/java/lightpath/com/ibuy/presentation/view/Principal/PrincipalActivity.java
+++ app/src/main/java/lightpath/com/ibuy/presentation/view/Principal/PrincipalActivity.java
@@ -1,15 +1,39 @@
 package lightpath.com.ibuy.presentation.view.Principal;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import lightpath.com.ibuy.R;
+import lightpath.com.ibuy.presentation.view.Favorites.FavoritesFragment;
+import lightpath.com.ibuy.presentation.view.Principal.entities.ViewPagerAdapter;
 
 public class PrincipalActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        tabLayout = (TabLayout) findViewById(R.id.principal_tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.principal_viewPager);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new FavoritesFragment(PrincipalActivity.this), "Fav");
+        viewPagerAdapter.addFragments(new FavoritesFragment(PrincipalActivity.this), "Fav");
+        viewPagerAdapter.addFragments(new FavoritesFragment(PrincipalActivity.this), "Fav");
+        viewPagerAdapter.addFragments(new FavoritesFragment(PrincipalActivity.this), "Fav");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.mipmap.ic_launcher);
+        tabLayout.getTabAt(1).setIcon(R.mipmap.ic_launcher);
+        tabLayout.getTabAt(2).setIcon(R.mipmap.ic_launcher);
+        tabLayout.getTabAt(3).setIcon(R.mipmap.ic_launcher);
+
     }
 }
