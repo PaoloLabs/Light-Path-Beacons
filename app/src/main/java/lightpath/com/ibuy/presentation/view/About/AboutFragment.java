6122b078646e6eb99f3582e14120b8a0962dfbe8
Janeth Martinez <jhanel.miss@gmail.com>
2017-02-19 23:25:58 -0400

    modificaciones en el about

--- app/src/main/java/lightpath/com/ibuy/presentation/view/About/AboutFragment.java
+++ app/src/main/java/lightpath/com/ibuy/presentation/view/About/AboutFragment.java
@@ -27,7 +27,7 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
     @Override
--- app/src/main/java/lightpath/com/ibuy/presentation/view/SplashActivity.java
+++ app/src/main/java/lightpath/com/ibuy/presentation/view/SplashActivity.java
@@ -1,15 +1,188 @@
 package lightpath.com.ibuy.presentation.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import lightpath.com.ibuy.R;
+import lightpath.com.ibuy.presentation.view.Principal.PrincipalActivity;
+import lightpath.com.ibuy.presentation.view.Tutorial.TutorialActivity;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private long splashDelay = 2000;
+    private ImageView imgSplash;
+    private TimerTask task;
+
+    private Timer t;
+    private int TimeCounter = 0;
+
+    private static final int MY_PERMISSIONS_1 = 1;
+    private static final int MY_PERMISSIONS_2 = 2;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        imgSplash = (ImageView) findViewById(R.id.imgSplash);
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_1);
+
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_2);
+
+            }
+        }
+
+
+        t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        TimeCounter++;
+                        switch (TimeCounter) {
+                            case 1:
+                                imgSplash.setBackgroundResource(R.drawable.splash_1);
+                                break;
+
+                            case 2:
+                                imgSplash.setBackgroundResource(R.drawable.splash_1);
+                                break;
+
+                            case 3:
+                                imgSplash.setBackgroundResource(R.drawable.splash_1);
+                                break;
+
+                            case 8:
+                                Intent mainIntent = new Intent().setClass(SplashActivity.this, TutorialActivity.class);
+                                startActivity(mainIntent);
+                                finish();
+                                t.cancel();
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                });
+
+            }
+        }, 1000, 500);
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                }
+                return;
+            }
+            case MY_PERMISSIONS_2: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Splash Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
