e324ce927a0bceefca4903b5d088cef90b1d9e4e
Janeth Martinez <jhanel.miss@gmail.com>
2017-02-21 11:04:21 -0400

    codeqr and generator

--- app/src/main/java/lightpath/com/ibuy/presentation/view/QReader/QReaderFragment.java
+++ app/src/main/java/lightpath/com/ibuy/presentation/view/QReader/QReaderFragment.java
@@ -1,17 +1,20 @@
 package lightpath.com.ibuy.presentation.view.QReader;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import lightpath.com.ibuy.R;
 
 public class QReaderFragment extends Fragment {
     private Context context;
+    Button gen, scan;
 
     private OnFragmentInteractionListener mListener;
 
@@ -25,9 +28,29 @@ public class QReaderFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        gen = (Button) container.findViewById(R.id.gen);
+        scan = (Button) container.findViewById(R.id.scan);
+
+        gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Intent gIntent = new Intent(QReaderFragment.this, GeneratorActivity.class);
+                startActivity(gIntent);
+            }
+        });
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rIntent = new Intent(QReaderFragment.this, ReaderActivity.class);
+                startActivity(rIntent);
+            }
+        });
+
+
+        return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
     @Override
